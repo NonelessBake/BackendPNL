abcd3e6d4096b7188324c2364ce7546ec265ddaa
NonelessBake <bakeferocious@gmail.com>
2023-10-21 05:07:57 +0700

    Add project

--- FirstStart/routers/index.js
+++
@@ -1,7 +0,0 @@
-const usersRouter = require("./users");
-const productsRouter = require("./products");
-const router = require("express").Router();
-router.use("/users", usersRouter);
-router.use("/products", productsRouter);
-
-module.exports = router;
